795ec924b8b86434537ade119d6391e94ef5f8ba
munirkizilcali <munir.kizilcali@gmail.com>
2018-07-15 09:55:06 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import CreateTodo from './components/todos/CreateTodo'
+import React, { Component } from "react";
+import CreateTodo from "./components/todos/CreateTodo";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CreateTodo store={this.props.store} />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<CreateTodo store={this.props.store} />
+			</div>
+		);
+	}
 }
 
 export default App;
